17bcc6406168e6466041a1e584840fcd7c4efa3a
Reza Ghanbari <rezapython@gmail.com>
2017-11-20 15:22:34 +0330

    knapsack problem

---
+++ knapsack.py
@@ -0,0 +1,24 @@
+# Reza Ghanbari
+# Advanced algorithm course
+# Knapsack problem using dynamic programming
+# Compatibiltiy with python 2 and 3
+
+def knapSack(W, wt, val, n):
+    K = [[0 for x in range(W+1)] for x in range(n+1)]
+ 
+    for i in range(n+1):
+        for w in range(W+1):
+            if i==0 or w==0:
+                K[i][w] = 0
+            elif wt[i-1] <= w:
+                K[i][w] = max(val[i-1] + K[i-1][w-wt[i-1]],  K[i-1][w])
+            else:
+                K[i][w] = K[i-1][w]
+ 
+    return K[n][W]
+
+val = [20, 30, 40]
+wt = [5, 10, 30]
+W = 30
+n = len(val)
+print(knapSack(W , wt , val , n))
